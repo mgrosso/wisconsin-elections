72cabb6aa68048c1cb0ceaadee8f8139d664d560
Matt Grosso <mgrosso@acm.org>
2019-07-07 23:45:18 -0700

    pull errata into a separate module

--- lib/join_equipment_results.rb
+++ lib/join_equipment_results.rb
@@ -2,12 +2,20 @@ module JoinEquipmentResultHelpers
   def joined
     result16s = ResultsParser2016.loadhash_by_county_city
     equipments = EquipmentParser.loadhash_by_county_city
-    alt_equipments = EquipmentParser.fixup_join_keys equipments
+    alt_equipments = fixes_for_missing equipments
     result16s.each_pair.map do |key, result_obj|
       ret = result_obj.dup
       ret[:equipment] = equipments[key] || alt_equipments[key]
       ret
     end
+  end
+
+  def fixes_for_missing(source_hash)
+    Hash[
+      Errata.join_fixes.each do |result_key, equipment_key|
+        [result_key, source_hash[equipment_key]]
+      end
+    ]
   end
 
   def join_fails
